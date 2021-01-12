1f81c6bbab42f5ebe624594cf18848b1a0420c48
hj <hyejuggang@gmail.com>
2021-01-12 20:16:03 +0900

    ADD various language

--- main.js
+++ main.js
@@ -8,3 +8,23 @@ toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
     icons.classList.toggle('active');
 });
+
+const koBtn = document.querySelector('.navbar__lang__ko');
+
+koBtn.addEventListener('click', ()=>{
+    document.querySelector('.navbar__menu__home').innerHTML = '홈';
+    document.querySelector('.navbar__menu__gallery').innerHTML = '사진첩';
+    document.querySelector('.navbar__menu__wedding').innerHTML = '결혼식 정보';
+    document.querySelector('.navbar__menu__faq').innerHTML = '자주묻는질문';
+    document.querySelector('.navbar__menu__booking').innerHTML = '예약';
+});
+
+const engBtn = document.querySelector('.navbar__lang__eng');
+
+engBtn.addEventListener('click', ()=>{
+    document.querySelector('.navbar__menu__home').innerHTML = 'Home';
+    document.querySelector('.navbar__menu__gallery').innerHTML = 'Gallery';
+    document.querySelector('.navbar__menu__wedding').innerHTML = 'Weddings';
+    document.querySelector('.navbar__menu__faq').innerHTML = 'FAQ';
+    document.querySelector('.navbar__menu__booking').innerHTML = 'Booking';
+});
